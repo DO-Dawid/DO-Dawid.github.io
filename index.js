44c596e11c531a35c093b82c0213278e74522908
Dawid Olejniczak <67868027+DO-Dawid@users.noreply.github.com>
2023-08-06 23:10:48 +0200

    last touches
    minor changes, new pictures

--- index.js
+++ index.js
@@ -100,28 +100,50 @@ function updatePrice() {
     document.getElementById("totalPrice").value = totalPrice.toLocaleString();
   }
 
-  function submitForm() {
-    var form = document.getElementById("quotationForm");
-    // Dodać logikę do wysłania formularza (np. AJAX request)
-    // Tutaj można użyć biblioteki, takiej jak axios lub fetch, aby wysłać formularz na serwer.
-    // Przykład użycia zapytania fetch:
-    /*
-    fetch("url_do_endpointu", {
-      method: "POST",
-      body: new FormData(form)
-    })
-    .then(response => {
-      // Obsługa odpowiedzi od serwera
-    })
-    .catch(error => {
-      // Obsługa błędu
-    });
-    */
-  }
-// window.addEventListener("load", function() {
-//     const loader = this.document.querySelector(".loader");
-//     loader.className += " hidden2";
-// })
+  // Pobierz formularz i przypisz mu zdarzenie submit
+var form = document.getElementById("kontakt-form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault(); // Zatrzymaj domyślne zachowanie formularza
+
+  // Pobierz wartości pól formularza
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var phone = document.getElementById("phone").value;
+
+  // Utwórz obiekt FormData i dodaj wartości pól formularza
+  var formData = new FormData();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("message", message);
+  formData.append("phone", phone);
+
+  // Utwórz nowe żądanie AJAX
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "adres_hosta/funkcja_email", true); // Zmień "adres_hosta/funkcja_email" na odpowiedni adres URL funkcji email na Twoim hoście
+
+  // Ustaw nagłówki żądania, jeśli są wymagane
+  // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  // xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+
+  // Zarejestruj funkcję obsługującą odpowiedź z serwera
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        // Sukces: obsłuż odpowiedź z serwera po pomyślnym przesłaniu e-maila
+        console.log("E-mail został wysłany!");
+        // Tutaj możesz dodać kod obsługujący sukces (np. wyświetlenie potwierdzenia)
+      } else {
+        // Błąd: obsłuż błąd odpowiedzi z serwera
+        console.error("Wystąpił błąd podczas wysyłania e-maila.");
+        // Tutaj możesz dodać kod obsługujący błąd (np. wyświetlenie komunikatu o błędzie)
+      }
+    }
+  };
+
+  // Wyślij żądanie AJAX z danymi formularza
+  xhr.send(formData);
+});
 
 
 
